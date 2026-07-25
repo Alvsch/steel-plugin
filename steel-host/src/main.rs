2b7481691de398140e8af8acdf2a6d64bcc8572c
Alvsch <alvsch@proton.me>
2026-07-25 20:43:20 +0200

    remove comment

--- steel-host/src/main.rs
+++ steel-host/src/main.rs
@@ -8,15 +8,6 @@ use tracing::Level;
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
-    // discover - find plugins in a directory with a valid config.toml
-    // resolve - further validate the config.toml version requirements, topo sort and build file table
-    // compile - compile plugin into bytecode
-    // execute - evaluates the init.lua file and returns on_enable/on_disable
-    // enable - runs the on_enable function
-    // runtime
-    // disable - runs the on_disable function
-    // teardown - unload the plugin and delete it's env
-
     let discovered_plugins = discover_plugins("examples");
     let resolved_plugins = resolve_plugins(discovered_plugins)?;
 
